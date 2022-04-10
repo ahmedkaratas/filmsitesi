d39dbda945a1037b8b789dfce11096ff6971a495
ahmedkaratas <02185076019@ogr.inonu.edu.tr>
2022-04-10 23:39:16 +0300

    Bean düzenlemesi

--- FilmSitesi/src/java/entity/Loglar.java
+++ FilmSitesi/src/java/entity/Loglar.java
@@ -4,10 +4,57 @@
  */
 package entity;
 
-/**
- *
- * @author celal
- */
+
 public class Loglar {
+    private int id;
+    private String kullaniciadi;
+    private String hareket;
+    public String ip;
+    public String tarih;
+    public String getHareket;
+
+    public Loglar(int aInt, String string, String string0, String string1) {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKullaniciadi() {
+        return kullaniciadi;
+    }
+
+    public void setKullaniciadi(String kullaniciadi) {
+        this.kullaniciadi = kullaniciadi;
+    }
+
+    public String getHareket() {
+        return hareket;
+    }
+
+    public void setHareket(String hareket) {
+        this.hareket = hareket;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getTarih() {
+        return tarih;
+    }
+
+    public void setTarih(String tarih) {
+        this.tarih = tarih;
+    }
     
 }

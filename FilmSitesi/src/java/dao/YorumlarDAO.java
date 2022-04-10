ed292c7796a45550fdddd8db3c0d0f4735ace4ee
CelalBinici7 <celalbinici150@gmail.com>
2022-04-11 00:44:37 +0300

    YorumlarDAO eklendi.

--- FilmSitesi/src/java/dao/YorumlarDAO.java
+++ FilmSitesi/src/java/dao/YorumlarDAO.java
@@ -1,15 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dao;
 
+import entity.Yorumlar;
+import entity.sss;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import util.DBConnection;
 
-/**
- *
- * @author celal
- */
+
 public class YorumlarDAO extends DBConnection{
-    
+    public void create(Yorumlar y) {
+        try {
+
+            Connection c = this.connect();
+            Statement st = c.createStatement();
+
+            String q = "insert into Yorumlar (yorum,tarih) values ('" + y.getYorum() + "','" + y.getTarih() + "')";
+            st.executeUpdate(q);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+     public void delete(Yorumlar y) {
+        try {
+
+            Connection c = this.connect();
+            Statement st = c.createStatement();
+
+            String q = "delete from Yorumlar where id = " + y.getKullanici_id();
+            st.executeUpdate(q);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+     public void update(Yorumlar y) {                
+        try {
+
+            Connection c = this.connect();
+            Statement st = c.createStatement();
+
+            String q = "update Yorumlar set yorum = '" + y.getYorum()  + "',tarih= '" + y.getTarih() + "' where id =" +y.getKullanici_id() ;
+            st.executeUpdate(q);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+     
+     public List<Yorumlar> getList() {
+        List<Yorumlar> list = new ArrayList<>();
+        try {
+            Connection c = this.connect();
+            Statement st = c.createStatement();
+
+            String q = "select * from Yorumlar";
+            ResultSet rs = st.executeQuery(q);
+            while (rs.next()) {
+                list.add(new Yorumlar(rs.getInt("kullanici_id"), rs.getInt("film_id"), rs.getString("yorum"),rs.getString("tarih")));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return list;
+    }
+     
 }

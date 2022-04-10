0dca7e517ccf41b2ba9ef83e06c3987ce652f8f7
ahmedkaratas <02185076019@ogr.inonu.edu.tr>
2022-04-11 00:03:08 +0300

    Bean düzenlemesi

--- FilmSitesi/src/java/dao/KullanıcılarDAO.java
+++ FilmSitesi/src/java/dao/KullanıcılarDAO.java
@@ -1,9 +1,68 @@
 
 package dao;
 
+import entity.Kullanıcılar;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import util.DBConnection;
 
 
-public class KullanıcılarDAO extends DBConnection{
+public class KullanıcılarDAO extends DBConnection {
     
+    public void create(Kullanıcılar s){
+        try{
+            Statement st = this.connect().createStatement();
+            String query = "insert into kullanıcılar (kullaniciadi) values ('"+s.getKullaniciadi+"','"+s.getAd+"','"+s.getSoyad+"','"+s.getEposta+"','"+s.getSifre+"')";
+            
+            st.executeUpdate(query);
+            
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    
+    }
+    
+    public void update(Kullanıcılar s){
+        try{
+            Statement st = this.connect().createStatement();
+            String query = "update kullanıcılar set kullaniciadi='"+s.getKullaniciadi()+"' where id="+s.getId();
+            
+            st.executeUpdate(query);
+            
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void delete(Kullanıcılar s){
+        try{
+            Statement st = this.connect().createStatement();
+            String query = "delete from kullanıcılar where id="+s.getId();
+            
+            st.executeUpdate(query);
+            
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public List<Kullanıcılar> getList(){
+        List<Kullanıcılar> list = new ArrayList<>();
+        try{
+            Statement st = this.connect().createStatement();
+            String query = "select * from sss";
+            
+            ResultSet rs = st.executeQuery(query);
+            
+            while (rs.next()){
+                list.add(new Kullanıcılar(rs.getInt("id"), rs.getString("hareket"), rs.getString("ip"), rs.getString("tarih")));
+            }
+            
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
 }

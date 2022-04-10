209bbfff2980061179369b49bd2b689e1c19ad79
ahmedkaratas <02185076019@ogr.inonu.edu.tr>
2022-04-11 00:03:22 +0300

    Bean düzenlemesi

--- FilmSitesi/src/java/entity/Kullanıcılar.java
+++ FilmSitesi/src/java/entity/Kullanıcılar.java
@@ -3,5 +3,70 @@ package entity;
 
 
 public class Kullanıcılar {
+
+    public String getId;
+    public String getAd;
+    public String getEposta;
+    public String getKullaniciadi;
+    public String getSifre;
+    public String getSoyad;
+
+    public Kullanıcılar(int aInt, String string, String string0, String string1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Kullanıcılar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getGetAd() {
+        return getAd;
+    }
+
+    public void setGetAd(String getAd) {
+        this.getAd = getAd;
+    }
+
+    public String getGetEposta() {
+        return getEposta;
+    }
+
+    public void setGetEposta(String getEposta) {
+        this.getEposta = getEposta;
+    }
+
+    public String getGetKullaniciadi() {
+        return getKullaniciadi;
+    }
+
+    public void setGetKullaniciadi(String getKullaniciadi) {
+        this.getKullaniciadi = getKullaniciadi;
+    }
+
+    public String getGetSifre() {
+        return getSifre;
+    }
+
+    public void setGetSifre(String getSifre) {
+        this.getSifre = getSifre;
+    }
+
+    public String getGetSoyad() {
+        return getSoyad;
+    }
+
+    public void setGetSoyad(String getSoyad) {
+        this.getSoyad = getSoyad;
+    }
+
+    public String getId() {
+        return null;
+    }
+
+    public String getKullaniciadi() {
+       
+        return null;
+       
+    }
     
 }

eb40a6f8d1d58e5118ff134f283d11dc0937a78f
ahmedkaratas <02185076019@ogr.inonu.edu.tr>
2022-04-11 00:18:48 +0300

    Bean düzenlemesi

---
+++ FilmSitesi/src/java/entity/Iletisim.java
@@ -0,0 +1,56 @@
+
+package entity;
+
+
+public class Iletisim {
+
+    public String getId;
+    public String getAd;
+    public String getEposta;
+    public String getSoyad;
+    public String getBaslik;
+    public String getMesaj;
+
+    public Iletisim(int aInt, String string, String string0, String string1) {
+        
+    }
+
+    public Iletisim() {
+       
+    }
+
+    public String getGetAd() {
+        return getAd;
+    }
+
+    public void setGetAd(String getAd) {
+        this.getAd = getAd;
+    }
+
+    public String getGetEposta() {
+        return getEposta;
+    }
+
+    public void setGetEposta(String getEposta) {
+        this.getEposta = getEposta;
+    }
+    
+    public String getGetSoyad() {
+        return getSoyad;
+    }
+
+    public void setGetSoyad(String getSoyad) {
+        this.getSoyad = getSoyad;
+    }
+
+    public String getId() {
+        return null;
+    }
+
+    public String getKullaniciadi() {
+       
+        return null;
+       
+    }
+    
+}

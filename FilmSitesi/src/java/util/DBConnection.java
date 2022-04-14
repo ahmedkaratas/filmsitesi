414a932712fb7b13462e284c7cc3e88808ac6d49
ahmedkaratas <02185076019@ogr.inonu.edu.tr>
2022-04-14 14:09:17 +0300

    .

--- FilmSitesi/src/java/util/DBConnection.java
+++ FilmSitesi/src/java/util/DBConnection.java
@@ -12,7 +12,7 @@ public class DBConnection {
         Connection c = null;
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Filmsitesi", "postgres", "12345");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/filmsitesi", "postgres", "12345");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());

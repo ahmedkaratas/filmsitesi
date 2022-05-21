5981125c5fa30fe39f2717bd4e4b9d762c891d78
CelalBinici7 <celalbinici150@gmail.com>
2022-05-21 17:37:36 +0300

    LoglarBean.java eklendi.

---
+++ FilmSitesi/src/java/controller/LoglarBean.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package controller;
+
+import dao.LoglarDAO;
+import entity.Loglar;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+
+
+@Named(value = "loglarBean")
+@SessionScoped
+public class LoglarBean implements Serializable {
+
+    private Loglar entity;
+    private LoglarDAO dao;
+    private List<Loglar> list;
+    
+    public LoglarBean() {
+        
+        
+    }
+     public void create() {
+        this.getDao().createLoglar(entity);
+        this.entity = new Loglar();
+    }
+
+    public void update() {
+        this.getDao().updateLoglar(entity);
+        this.entity = new Loglar();
+        
+    }
+
+    public void delete(Loglar l) {
+        this.getDao().deleteLoglar(l);
+    }
+
+    public Loglar getEntity() {
+        if (this.entity == null) {
+            this.entity = new Loglar();
+        }
+        return entity;
+    }
+
+    public void setEntity(Loglar entity) {
+        this.entity = entity;
+    }
+
+    public LoglarDAO getDao() {
+        if (this.dao == null) {
+            this.dao = new LoglarDAO();
+        }
+        return dao;
+    }
+
+    public void setDao(LoglarDAO dao) {
+        this.dao = dao;
+    }
+
+    public List<Loglar> getList() {
+        this.list = this.getDao().getLoglarList();
+        return list;
+    }
+
+    public void setList(List<Loglar> list) {
+        this.list = list;
+    }
+    
+}

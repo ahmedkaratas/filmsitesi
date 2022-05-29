cacf408db4f351b50c68ae05f8b799ea020e4a07
CelalBinici7 <celalbinici150@gmail.com>
2022-05-29 10:19:06 +0300

    FileServlet.java eklendi.

---
+++ FilmSitesi/src/java/util/FileServlet.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package util;
+
+import controller.DocumentBean;
+import jakarta.inject.Inject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.nio.file.Files;
+
+/**
+ *
+ * @author celal
+ */
+@WebServlet(name = "FileServlet", urlPatterns = {"/file/*"})
+public class FileServlet extends HttpServlet {
+
+    @Inject
+    private DocumentBean db;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String file = request.getPathInfo();
+        File f = new File(db.getUploadTo()+file);
+
+        Files.copy(f.toPath(), response.getOutputStream());
+    }
+
+}

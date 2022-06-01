394067ac42ba3e2b1d3d2bb927d1adcf65bf959a
erencanar <02185076006@ogr.inonu.edu.tr>
2022-06-01 21:54:13 +0300

    Hatalar düzeltildi.

--- FilmSitesi/src/java/dao/FilmDAO.java
+++ FilmSitesi/src/java/dao/FilmDAO.java
@@ -14,28 +14,25 @@ import entity.Kategori;
 
 import util.DBConnection;
 
-
-
 public class FilmDAO extends DBConnection {
 
-    
     public void createFilm(Film f) {
         try {
 
             Statement st = this.getConnection().createStatement();
             String q = "insert into film (ad,tur,vizyon,sure,ulke,puan,yassiniri,filmlinki,aciklama,gorsel) values ('" + f.getAd() + "','" + f.getTur() + "','" + f.getVizyon() + "','" + f.getSure() + "','" + f.getUlke() + "'," + f.getPuan() + ",'" + f.getYassiniri() + "','" + f.getFilmlinki() + "','" + f.getAciklama() + "','" + f.getGorsel() + "')";
             st.executeUpdate(q);
-            
+
             ResultSet rs = st.executeQuery("select max(filmid) as mid from film");
             rs.next();
-            
+
             int film_id = rs.getInt("mid");
-            
-            for ( Kategori cat : f.getCategories() ){
-                q = "insert into filmkategori (film_id, kategori_id) values ("+film_id+","+cat.getId()+")";
+
+            for (Kategori cat : f.getCategories()) {
+                q = "insert into filmkategori (film_id, kategori_id) values (" + film_id + "," + cat.getId() + ")";
                 st.executeUpdate(q);
             }
-            
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -46,10 +43,12 @@ public class FilmDAO extends DBConnection {
         try {
 
             Statement st = this.getConnection().createStatement();
-            st.executeUpdate("delete from filmkategori where film_id="+f.getFilmid());
+            st.executeUpdate("delete from filmkategori where film_id=" + f.getFilmid());
+            st.executeUpdate("delete from yorumlar where filmid=" + f.getFilmid());
+            st.executeUpdate("delete from fotograflar where filmid=" + f.getFilmid());
             String q = "delete from film  where filmid =" + f.getFilmid();
             st.executeUpdate(q);
-            
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -62,14 +61,14 @@ public class FilmDAO extends DBConnection {
             Statement st = this.getConnection().createStatement();
             String q = "update film set ad ='" + f.getAd() + "', tur='" + f.getTur() + "', vizyon='" + f.getVizyon() + "', sure='" + f.getSure() + "', ulke='" + f.getUlke() + "', puan='" + f.getPuan() + "', yassiniri='" + f.getYassiniri() + "', filmlinki='" + f.getFilmlinki() + "', aciklama='" + f.getAciklama() + "', gorsel='" + f.getGorsel() + "' where filmid =" + f.getFilmid();
             st.executeUpdate(q);
-            
-            st.executeUpdate("delete from filmkategori where film_id="+f.getFilmid());
-            
-            for ( Kategori cat : f.getCategories() ){
-                q = "insert into filmkategori (film_id, kategori_id) values ("+f.getFilmid()+","+cat.getId()+")";
+
+            st.executeUpdate("delete from filmkategori where film_id=" + f.getFilmid());
+
+            for (Kategori cat : f.getCategories()) {
+                q = "insert into filmkategori (film_id, kategori_id) values (" + f.getFilmid() + "," + cat.getId() + ")";
                 st.executeUpdate(q);
             }
-            
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -78,8 +77,8 @@ public class FilmDAO extends DBConnection {
 
     public List<Film> getFilmList(int page, int pageSize) {
         List<Film> FilmList = new ArrayList<>();
-        
-        int start = (page-1)*pageSize;
+
+        int start = (page - 1) * pageSize;
 
         try {
 
@@ -87,7 +86,7 @@ public class FilmDAO extends DBConnection {
             String q = "select * from film order by filmid asc limit '" + pageSize + "' offset '" + start + "'";
             ResultSet rs = st.executeQuery(q);
             while (rs.next()) {
-                
+
                 FilmList.add(new Film(rs.getInt("Filmid"), this.getPostCategories(rs.getInt("filmid")), rs.getString("ad"), rs.getString("tur"), rs.getString("vizyon"), rs.getString("sure"), rs.getString("ulke"), rs.getDouble("puan"), rs.getString("yassiniri"), rs.getString("filmlinki"), rs.getString("aciklama"), rs.getString("gorsel")));
 
             }
@@ -98,7 +97,7 @@ public class FilmDAO extends DBConnection {
 
         return FilmList;
     }
-    
+
     public List<Film> getFilmListFull() {
         List<Film> FilmList = new ArrayList<>();
 
@@ -108,7 +107,7 @@ public class FilmDAO extends DBConnection {
             String q = "select * from film";
             ResultSet rs = st.executeQuery(q);
             while (rs.next()) {
-                
+
                 FilmList.add(new Film(rs.getInt("Filmid"), this.getPostCategories(rs.getInt("filmid")), rs.getString("ad"), rs.getString("tur"), rs.getString("vizyon"), rs.getString("sure"), rs.getString("ulke"), rs.getDouble("puan"), rs.getString("yassiniri"), rs.getString("filmlinki"), rs.getString("aciklama"), rs.getString("gorsel")));
 
             }
@@ -119,7 +118,7 @@ public class FilmDAO extends DBConnection {
 
         return FilmList;
     }
-    
+
     public List<Film> getListTekFilm(int filmid) {
         List<Film> FilmList = new ArrayList<>();
 
@@ -129,7 +128,7 @@ public class FilmDAO extends DBConnection {
             String q = "select * from film where filmid=" + filmid;
             ResultSet rs = st.executeQuery(q);
             while (rs.next()) {
-                
+
                 FilmList.add(new Film(rs.getInt("Filmid"), this.getPostCategories(rs.getInt("filmid")), rs.getString("ad"), rs.getString("tur"), rs.getString("vizyon"), rs.getString("sure"), rs.getString("ulke"), rs.getDouble("puan"), rs.getString("yassiniri"), rs.getString("filmlinki"), rs.getString("aciklama"), rs.getString("gorsel")));
 
             }
@@ -140,12 +139,12 @@ public class FilmDAO extends DBConnection {
 
         return FilmList;
     }
-    
-    public List<Kategori> getPostCategories(int film_id){
+
+    public List<Kategori> getPostCategories(int film_id) {
         List<Kategori> KategoriList = new ArrayList<>();
         try {
             Statement st = this.getConnection().createStatement();
-            String q = "select * from kategori where id in (select kategori_id from filmkategori where film_id="+film_id+")";
+            String q = "select * from kategori where id in (select kategori_id from filmkategori where film_id=" + film_id + ")";
             ResultSet rs = st.executeQuery(q);
             while (rs.next()) {
                 KategoriList.add(new Kategori(rs.getInt("id"), rs.getString("adi")));
@@ -158,7 +157,7 @@ public class FilmDAO extends DBConnection {
 
         return KategoriList;
     }
-    
+
     public int count() {
         int count = 0;
 
@@ -169,7 +168,6 @@ public class FilmDAO extends DBConnection {
             ResultSet rs = st.executeQuery(q);
             rs.next();
             count = rs.getInt("film_count");
-            
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -185,7 +183,7 @@ public class FilmDAO extends DBConnection {
         try {
 
             Statement st = this.getConnection().createStatement();
-            String q = "select * from film where filmid="+filmid;
+            String q = "select * from film where filmid=" + filmid;
             ResultSet rs = st.executeQuery(q);
             while (rs.next()) {
                 f = new Film(rs.getInt("filmid"), this.getPostCategories(rs.getInt("filmid")), rs.getString("ad"), rs.getString("tur"), rs.getString("vizyon"), rs.getString("sure"), rs.getString("ulke"), rs.getDouble("puan"), rs.getString("yassiniri"), rs.getString("filmlinki"), rs.getString("aciklama"), rs.getString("gorsel"));
@@ -197,7 +195,5 @@ public class FilmDAO extends DBConnection {
         }
         return f;
     }
-
-    
 
 }
